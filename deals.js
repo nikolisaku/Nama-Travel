39c1950561bac7b308a0df4a9f919c5837ad7e13
nikolisaku <nikolisaku05@gmail.com>
2025-11-24 14:37:19 +0100

    Update deals.js

--- deals.js
+++ deals.js
@@ -1,1 +1,11 @@
-console.log("Deals page loaded successfully.");
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+        const destinationName = card.querySelector(".card-title").textContent;
+
+        // save selected deal
+        localStorage.setItem("selectedDeal", destinationName);
+
+        // redirect to dashboard
+        window.location.href = "dashboard.html";
+    });
+});
